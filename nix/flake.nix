71fbe1bce54f4e8c4a1b1d4f85518d777931c10d
Nikita Mitasov <me@ch4og.com>
2026-03-13 14:07:23 +0300

    chore(nix): add dev env flake

---
+++ nix/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "plane-telegram-webhooks";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [nodejs_24 corepack git reuse docker];
+      };
+    });
+}
