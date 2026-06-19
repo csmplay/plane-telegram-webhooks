9f79b68942a28e3f905a7c11a64ff387c958efa3
Nikita Mitasov <me@ch4og.com>
2026-06-19 21:19:32 +0300

    chore(logger): remove dead code

--- app/logger.js
+++ app/logger.js
@@ -1,9 +1,6 @@
 // SPDX-FileCopyrightText: 2026 CyberSport Masters <git@csmpro.ru>
 // SPDX-License-Identifier: AGPL-3.0-only
 
-const fs = require('fs');
-const path = require('path');
-
 const LOG_LEVELS = ['ERROR', 'WARN', 'INFO', 'DEBUG'];
 const DEFAULT_LOG_LEVEL = 'INFO';
 const getLogLevel = () => {
